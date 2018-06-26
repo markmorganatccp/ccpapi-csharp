fd2e3cc4696f833919c7573b656ab682a4729130
markmorgan <markmorgan@cloudcommercepro.com>
2018-06-26 10:18:46 +0100

    added orders class

---
+++ CloudCommerceProApiClient/Orders.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudCommerceProApiClient.CloudCommercePro.Orders;
+
+namespace CloudCommerceProApiClient
+{
+    /// <summary>
+    /// Makes Service Calls to CCP API Orders endpoint
+    /// </summary>
+    public class Orders
+    {
+        /// <summary>
+        /// Orders Client
+        /// </summary>
+        public CCPApiOrderServiceClient OrdersClient { get { return new CCPApiOrderServiceClient(); } }
+
+
+
+    }
+
+}
