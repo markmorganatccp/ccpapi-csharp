f9c632a6007d1872bf54aa258019e9befe106932
markmorgan <markmorgan@cloudcommercepro.com>
2018-06-26 10:34:24 +0100

    new objects

---
+++ CloudCommerceProApiClient/Customers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudCommerceProApiClient.CloudCommercePro.Customers;
+
+namespace CloudCommerceProApiClient
+{
+    /// <summary>
+    /// 
+    /// </summary>
+  public  class Customers
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public CCPApiCustomerServiceClient CustomersClient { get { return new CCPApiCustomerServiceClient(); } }
+
+
+    }
+}
---
+++ CloudCommerceProApiClient/ProductRanges.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudCommerceProApiClient.CloudCommercePro.ProductRanges;
+
+namespace CloudCommerceProApiClient
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProductRanges
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public CCPProductRangesClient ProductRangesClient { get { return new CCPProductRangesClient(); } }
+
+    }
+
+}
